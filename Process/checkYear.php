f0f7c8d8549c5000b6854974628d0e271afb204c
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-03-18 10:13:48 +0800

    Create checkYear.php

---
+++ Process/checkYear.php
@@ -0,0 +1,15 @@
+<?php
+	$user = $_GET['user'];
+	$con = mysqli_connect("localhost","root","","onlinegrading");
+	if(!$con){
+		echo "Failed connecting to the database";
+	}
+	
+	$sql1 = mysqli_query($con,"SELECT * FROM `studentlist` where `studentNumber` = '$user'");
+	
+	while($row=mysqli_fetch_array($sql1)){
+		$year = $row['year'];
+	}
+	
+	echo $year;
+?>
