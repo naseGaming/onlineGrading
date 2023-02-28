ce3eb9ce33ae9cedb65973edf1683ec7ece7f3de
Roland <regacho800@gmail.com>
2023-03-01 01:36:58 +0800

    Removed unnecessary console.log

--- error_pages/script.js
+++ error_pages/script.js
@@ -6,8 +6,6 @@ $(() => {
     let error_code = urlMessage[0].split("=")
     let error_text = urlMessage[1].split("=")
 
-    console.log(error_code)
-
     $("#error_code").html(error_code[1])
     $("#error_text").html(error_text[1].replace("%20", " "))
 })
