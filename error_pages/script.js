082cc5b9c2cf4c20fde7e8fd77ceb92db5fcf23d
Roland <regacho800@gmail.com>
2023-03-03 16:27:44 +0800

    added http error 403 handling

--- error_pages/script.js
+++ error_pages/script.js
@@ -15,6 +15,10 @@ $(() => {
     
     $("#error_code").html(render)
     switch(parseInt(error_code[1])) {
+        case 403: {
+            message = "Houston, we don't have enough clearance for that over."
+            break
+        }
         case 404: {
             message = "Houston, we cannot find the web page you're requesting over."
             break
